0c8e8e1767fd117013619739a187eb928d305578
Skinyan <ydamelio@outlook.de>
2024-04-19 10:21:55 +0200

    Composition API training

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ name }}</h1>
+  <input type="text" v-model="name" />
+  <button @click="placeOrder">ORdEr</button>
+  <YummyMeal
+    :name="meal.name"
+    :price="meal.price"
+    @addToCart="addItemtToCart"
+  />
+</template>
+
+<script>
+import YummyMeal from "./components/YummyMeal.vue";
+import { reactive, ref } from "vue";
+
+export default {
+  components: { YummyMeal },
+  setup() {
+    const name = ref("The Snazzy Burger");
+    const meal = reactive({ name: "Hamburger", price: 5 });
+    const placeOrder = () => alert("your order is placed");
+    const addItemtToCart = (item) => alert(`One ${item} added to the cart!`);
+
+    return { name, placeOrder, addItemtToCart, meal };
+  },
+};
+</script>
